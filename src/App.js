72413c8fa14cf7845c254904300532a5a8c76f3a
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-29 20:34:42 +0530

    added basic skeleton for MatchPage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import { TeamPage } from './pages/TeamPage';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/teams/:teamName" element={<TeamPage/>} />
-        </Routes>
-       
+          <Routes>
+            <Route path="/teams/:teamName/matches/:year" element={<MatchPage/>} />
+          </Routes>
+          <Routes>
+            <Route path="/teams/:teamName" element={<TeamPage/>} />
+          </Routes>
       </Router>
     </div>
   );
