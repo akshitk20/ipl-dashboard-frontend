84aa1538e3fd4ff8bc21c46873e96bf4176cd4dd
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-29 19:36:19 +0530

    added changes for MatchDetail card

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -3,7 +3,8 @@ import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const TeamPage = () => {
-    const [team, setTeam] = useState();
+    const [team, setTeam] = useState({matches:[]});
+    
     useEffect(() => {
         const fetchMatches = async () => {
             const response = await fetch('http://localhost:8080/team/Chennai Super Kings');
@@ -13,14 +14,12 @@ export const TeamPage = () => {
         };
         fetchMatches();
     },[]);
-
+    console.log(team);
     return (
         <div className="TeamPage">
          <h1>{team.teamName}</h1>
-         <MatchDetailCard/>
-         <MatchSmallCard/>
-         <MatchSmallCard/>
-         <MatchSmallCard/>
+         <MatchDetailCard match={team.matches[0]}/>
+         {team.matches.slice(1).map((match) => <MatchSmallCard match={match}/>) }
         </div>
       );
 }
