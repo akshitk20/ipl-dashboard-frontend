f665e7aea62222461e04b5593db5be0131307408
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-29 19:54:50 +0530

    add Routing for team page

--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -1,13 +1,15 @@
 import React,{useEffect,useState} from "react";
 import { MatchDetailCard } from "../components/MatchDetailCard";
 import { MatchSmallCard } from "../components/MatchSmallCard";
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
     const [team, setTeam] = useState({matches:[]});
-    
+    const {teamName}  = useParams();
+
     useEffect(() => {
         const fetchMatches = async () => {
-            const response = await fetch('http://localhost:8080/team/Chennai Super Kings');
+            const response = await fetch(`http://localhost:8080/team/${teamName}`);
             const data = await response.json();
             console.log(data);
             setTeam(data);
@@ -15,6 +17,9 @@ export const TeamPage = () => {
         fetchMatches();
     },[]);
     console.log(team);
+
+    if (!team || !team.teamName) return <h1>Team not found</h1>;
+
     return (
         <div className="TeamPage">
          <h1>{team.teamName}</h1>
