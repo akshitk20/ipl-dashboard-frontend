286a5ec4e71354371f820247e673c48140c9cafc
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-30 20:57:10 +0530

    completed matchapi data load from api

--- src/pages/MatchPage.js
+++ src/pages/MatchPage.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MatchDetailCard } from "../components/MatchDetailCard";
 
 export const MatchPage = () => {
+    const [matches, setMatches] = useState([]);
+
+    useEffect(() => {
+        const fetchMatches = async () => {
+            const response = await fetch('http://localhost:8080/team/Delhi Capitals/matches?year=2023');
+            const data = await response.json();
+            console.log(data);
+            setMatches(data);
+        }
+        fetchMatches();
+   
+    },[]);
+
     return (
         <div className="MatchPage">
             <h1>Match Page</h1>
+            {matches.map((match) => <MatchDetailCard teamName="Delhi Capitals" match={match}/>) }
         </div>
     )
 }
