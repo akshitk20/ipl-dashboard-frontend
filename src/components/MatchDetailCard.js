c0f00aa62c2db8cb62b83fbb83a79b1a9fa21149
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-31 17:38:26 +0530

    added more css for matchdetail and Team Page

--- src/components/MatchDetailCard.js
+++ src/components/MatchDetailCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import './MatchDetailCard.scss';
 
 export const MatchDetailCard = ({teamName, match}) => {
     if (!match) return null;
@@ -8,11 +9,15 @@ export const MatchDetailCard = ({teamName, match}) => {
 
     return (
         <div className="MatchDetailCard">
-         <h3>Latest Matches</h3>
-         <h1>vs <Link to={otherTeamRoute}>{otherTeam}</Link></h1>
-         <h2>{match.date}</h2>
-         <h3>at {match.venue}</h3>
-         <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+         <span className="vs">vs</span>
+         <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+         <h2 className="match-date">{match.date}</h2>
+         <h3 className="match-venue">at {match.venue}</h3>
+         <h3 className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+         <h3>First Innings</h3>
+         <p>{match.team1}</p>
+         <h3>Second Innings</h3>
+         <p>{match.team2}</p>
         </div>
       );
 }
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -24,12 +24,13 @@ export const TeamPage = () => {
     return (
         <div className="TeamPage">
             <div className="team-name-section">
-                <h1>{team.teamName}</h1>
+                <h1 className="team-name">{team.teamName}</h1>
             </div>
             <div className="win-loss-section">
                 Wins / Losses
             </div>
             <div className="match-detail-section">
+                <h3>Latest Matches</h3>
                 <MatchDetailCard teamName={team.teamName} match={team.matches[0]}/>
             </div>
             {team.matches.slice(1).map((match) => <MatchSmallCard teamName={team.teamName} match={match}/>)}
