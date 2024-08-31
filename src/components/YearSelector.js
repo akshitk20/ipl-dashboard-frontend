e69122b4cb1a18a9e855eabeb5a873f57b4d9974
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-31 20:47:54 +0530

    completed yearselector component

--- src/components/YearSelector.js
+++ src/components/YearSelector.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './YearSelector.scss';
 
 export const YearSelector = ({teamName}) => {
 
@@ -12,7 +13,7 @@ export const YearSelector = ({teamName}) => {
     }
 
     return (
-        <ol>
+        <ol className="YearSelector">
             {years.map(year => 
                 <li>
                     <Link to={`/teams/${teamName}/matches/${year}`}>{year}</Link>
