9b23a74c0d5e64de6d2bc16a0d2fc5da99c4d93c
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-31 17:55:28 +0530

    added css for MatchDetail card

--- src/components/MatchDetailCard.js
+++ src/components/MatchDetailCard.js
@@ -9,15 +9,23 @@ export const MatchDetailCard = ({teamName, match}) => {
 
     return (
         <div className="MatchDetailCard">
-         <span className="vs">vs</span>
-         <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
-         <h2 className="match-date">{match.date}</h2>
-         <h3 className="match-venue">at {match.venue}</h3>
-         <h3 className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
-         <h3>First Innings</h3>
-         <p>{match.team1}</p>
-         <h3>Second Innings</h3>
-         <p>{match.team2}</p>
+            <div>
+                <span className="vs">vs</span>
+                <h1><Link to={otherTeamRoute}>{otherTeam}</Link></h1>
+                <h2 className="match-date">{match.date}</h2>
+                <h3 className="match-venue">at {match.venue}</h3>
+                <h3 className="match-result">{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
+            </div>
+            <div className="additional-detail">
+                <h3>First Innings</h3>
+                <p>{match.team1}</p>
+                <h3>Second Innings</h3>
+                <p>{match.team2}</p>
+                <h3>Man of the match</h3>
+                <p>{match.playerOfMatch}</p>
+                <h3>Umpires</h3>
+                <p>{match.umpire1}, {match.umpire2}</p>
+            </div>
         </div>
       );
 }
